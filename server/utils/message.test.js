7ab8bd98a8d006ebbcbafd61b2796d7fcf64daef
= <rdenman@Robs-MacBook-Air.local>
2018-12-23 10:20:30 -0600

    add geolocation support

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const { generateMessage } = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -10,5 +10,20 @@ describe('generateMessage', () => {
 
     expect(typeof message.createdAt).toBe('number');
     expect(message).toMatchObject({ from, text });
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location message object', () => {
+    const from = 'Person';
+    const lat = 123;
+    const lng = 321;
+    const message = generateLocationMessage(from, lat, lng);
+
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toMatchObject({
+      from,
+      url: `https://www.google.com/maps?q=${lat},${lng}`
+    });
   });
 });
